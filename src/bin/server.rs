ae9d6d3d83f4057163784488e4e55eb3e85f53dc
Deep Majumder <deep.majumder2019@gmail.com>
2022-02-21 23:15:08 +0530

    Trying to read in user data and continue

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,16 @@
 use std::{
+    collections::HashMap,
+    error::Error,
     io::{BufRead, Write},
     net::TcpListener,
+    path::Path,
 };
 
 use colored::*;
+use csv::ReaderBuilder;
 use log::{debug, error, info, warn};
 use mini_ftp::{BufTcpStream, Command};
+use serde::Deserialize;
 
 const IP_ADDR: &str = "127.0.0.1";
 const PORT: u16 = 25000;
@@ -25,6 +30,19 @@ fn main() {
         .write_style(env_logger::WriteStyle::Always)
         .init();
 
+    let info = if let Some(user_filename) = std::env::args().next() {
+        match parse_user_info(user_filename) {
+            Ok(info) => info,
+            Err(err) => {
+                error!("Failed to read user info file: {}", err);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        error!("Expected: <user-filename>");
+        std::process::exit(1);
+    };
+
     let listener = match TcpListener::bind((IP_ADDR, PORT)) {
         Ok(listener) => listener,
         Err(err) => {
@@ -43,7 +61,7 @@ fn main() {
         };
         debug!("Accepted connection to {}", addr);
         let stream = BufTcpStream::new(stream);
-        let mut ctx = Context::new(stream);
+        let mut ctx = Context::new(stream, &info);
 
         // Now we have a connection
         // Listen for commands, quit if connection closed
@@ -66,7 +84,11 @@ fn main() {
             };
 
             match &cmd {
-                Command::User(_) => ctx.handle_user(&cmd),
+                Command::User(name) => {
+                    if !ctx.handle_user(name) {
+                        break;
+                    }
+                }
                 Command::Password(_) => todo!(),
                 Command::Cd(_) => todo!(),
                 Command::Dir => todo!(),
@@ -83,16 +105,65 @@ fn main() {
 }
 
 #[derive(Debug)]
-struct Context {
+struct Context<'info> {
     stream: BufTcpStream,
+    info: &'info HashMap<String, UserInfo>,
+    expected_pass: Option<&'info str>,
+    state: ContextState,
 }
 
-impl Context {
-    fn new(stream: BufTcpStream) -> Self {
-        Self { stream }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ContextState {
+    Connected,
+    UserSent,
+    Ready,
+}
+
+impl<'info> Context<'info> {
+    fn new(stream: BufTcpStream, info: &'info HashMap<String, UserInfo>) -> Self {
+        Self {
+            stream,
+            info,
+            expected_pass: None,
+            state: ContextState::Connected,
+        }
     }
 
-    fn handle_user(&mut self, cmd: &Command) {
-        todo!("Handle user!");
+    fn handle_user(&mut self, name: &str) -> bool {
+        if self.state != ContextState::Connected {
+            if let Err(err) = self.stream.write_all(b"600\0") {
+                info!("{}", err);
+                return false;
+            }
+            return true;
+        }
+        if let Some(user_info) = self.info.get(name) {
+            self.state = ContextState::UserSent;
+            self.expected_pass = Some(&user_info.pass);
+            if let Err(err) = self.stream.write_all(b"200\0") {
+                info!("{}", err);
+                return false;
+            }
+        } else if let Err(err) = self.stream.write_all(b"500\0") {
+            info!("{}", err);
+            return false;
+        }
+        true
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct UserInfo {
+    name: String,
+    pass: String,
+}
+
+fn parse_user_info<P: AsRef<Path>>(path: P) -> Result<HashMap<String, UserInfo>, Box<dyn Error>> {
+    let mut info = HashMap::new();
+    let mut rdr = ReaderBuilder::new().has_headers(false).from_path(path)?;
+    for row in rdr.deserialize() {
+        let user_info: UserInfo = row?;
+        info.insert(user_info.name.clone(), user_info);
+    }
+    Ok(info)
 }
